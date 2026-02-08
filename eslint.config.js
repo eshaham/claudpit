0505dcc88c470b2f95d815b588de53578b23df8e
Elad Shaham <7040645+eshaham@users.noreply.github.com>
2026-02-08 13:12:04 +0200

    Add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+  }
+);
